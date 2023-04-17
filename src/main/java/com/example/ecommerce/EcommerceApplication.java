a0f33a9637170b84baa036c2ea9a20277ca67734
jung <102740808+Jung-eee@users.noreply.github.com>
2023-04-17 17:15:18 +0900

    gradle 변경

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ecoomerce;
+package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EcoomerceApplication {
+public class EcommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcoomerceApplication.class, args);
+        SpringApplication.run(EcommerceApplication.class, args);
     }
 
 }
